143b1edfe4dcc1217d3faa114583f698ce4c6e01
Ryan Yogan <ryanyogan@gmail.com>
2015-12-18 08:17:15 -0600

    added a couple basic dependencies for debugging, sample api end point

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 3000;
+var router = express.Router();
+
+router.get('/', function(req, res) {
+  debugger;
+  res.json({ message: 'You are running dangerously low on beer!' });
+});
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('Insert beer on port ' + port);
